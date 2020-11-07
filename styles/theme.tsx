4ec486a489efc1ba2a528e8e789980341c5f2708
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2020-11-07 19:49:56 +1300

    Header styling adjustments

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,20 +1,21 @@
 export default {
     header: {
-        background: 'red',
-        color: 'blue',
+        padding: '20px 0px 20px 10px',
+        background: 'black',
+        color: 'white',
     },
     header_layout: {
         listStyleType: 'none',
         overflow: 'hidden',
         margin: 0,
-        padding: 0,
-        paddingTop: '5px',
-        paddingBottom: '5px',
+        padding: '5px 0px 5px 0px',
     },
     header_buttons: {
         display: 'inline',
         textDecoration: 'none',
         padding: '10px',
+        fontSize: '15px',
+        fontWeight: 'bold' as 'bold',
     },
     buttons: {
         colors: {

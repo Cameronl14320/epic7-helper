be9e01252117bcd0dba6c13d786e1a9ed16307f3
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2020-11-09 17:22:12 +1300

    formatting change

--- pages/Gear/gear.tsx
+++ pages/Gear/gear.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box, Text } from 'rebass'
-import gearStyle from './gearstyle'
+import gearStyle from './gearStyle'
 
 export default function Gear() {
     return (

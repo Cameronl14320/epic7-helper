92a8c1410b8429602265dfa14d31fbca4e16cc59
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2020-11-19 13:37:00 +1300

    added new section to select substat

--- components/shared/stats/gearSub.tsx
+++ components/shared/stats/gearSub.tsx
@@ -21,7 +21,20 @@ export default class GearSub extends Component<{}, {id : string, stat : statObje
         this.stat = props.stat;
     }
 
+    changeVisibility() {
+        var panel = document.getElementById(this.id + "Change");
+        var state = panel.style.visibility == "hidden";
+        if (state) {
+            panel.style.visibility = "visible";
+            panel.style.display = "block";
+        } else {
+            panel.style.visibility = "hidden";
+            panel.style.display = "none";
+        }
+    }
+
     changeStat() {
+        /*
         var totalSub = subArray.length;
         var index : number = subArray.indexOf(this.stat) + 1;
         if (index >= totalSub) {
@@ -30,22 +43,34 @@ export default class GearSub extends Component<{}, {id : string, stat : statObje
         this.stat = subArray[index];
 
         document.getElementById(this.id).textContent = this.stat.name;
+        */
     }
 
     render() {
         return (
-            <>
+            <Box style={{
+                MozUserSelect: "none",
+                WebkitUserSelect: "none",
+                msUserSelect: "none"
+            }}>
                 <Box sx={{
                     display: 'inline-grid',
+                    background: 'white',
                 }}>
-                    <Box id={this.id} sx={subStyle.style} onClick={() => this.changeStat()}>
+                    <Box id={this.id} sx={subStyle.style} onClick={() => this.changeVisibility()}>
                         {this.stat.name}
                     </Box>
                     <Box id="Amount" sx={subStyle.style}>
                         Two
                     </Box>
                 </Box>
-            </>
+                <Box id ={this.id + "Change"}sx={{
+                    visibility: 'hidden',
+                    display: 'none',
+                }}>
+                    {this.id + "Change"}
+                </Box>
+            </Box>
         )
     }
 }
